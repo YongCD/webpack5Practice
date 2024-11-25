523618473c2a34a32b19b209eda602f06ee3490e
张永霖 <2678464408@qq.com>
2024-11-25 18:45:20 +0800

    build(06-manage-assets): 添加对 Less 文件的支持
    - 修改 webpack.config.js 文件
    - 增加对 .less 文件的测试规则
    - 在加载器中添加 less-loader 以处理 Less 文件

--- 06-manage-assets/webpack.config.js
+++ 06-manage-assets/webpack.config.js
@@ -47,8 +47,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(css|less)$/i,
+        use: ['style-loader', 'css-loader', 'less-loader'],
       },
     ],
   }
